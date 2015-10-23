161949123bb4ac083717a27fd4fc8004fdca232f
anchen.li <anchen.li@vedaleon.com>
2015-10-23 11:24:10 +1100

    init

---
+++ SAMLLib/Utils/Base64Encoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMLSPLib.Utils
+{
+    public class Base64Encoder
+    {
+        public string GetBase64EncodeStr(string rawStr)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(rawStr);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
---
+++ SAMLLib/Utils/SigningXMLHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SAMLSPLib.Utils
+{
+    public class SigningXMLHelper
+    {
+
+        public string SignXml(XmlDocument Document, X509Certificate2 cert)
+        {
+            SignedXml signedXml = new SignedXml(Document);
+            signedXml.SigningKey = cert.PrivateKey;
+            // Create a reference to be signed.
+            Reference reference = new Reference();
+            reference.Uri = "";
+
+            // Add an enveloped transformation to the reference.            
+            XmlDsigEnvelopedSignatureTransform env =
+               new XmlDsigEnvelopedSignatureTransform(true);
+            reference.AddTransform(env);
+
+            //canonicalize
+            XmlDsigC14NTransform c14t = new XmlDsigC14NTransform();
+            reference.AddTransform(c14t);
+
+            KeyInfo keyInfo = new KeyInfo();
+            KeyInfoX509Data keyInfoData = new KeyInfoX509Data(cert);
+          
+            keyInfo.AddClause(keyInfoData);
+            signedXml.KeyInfo = keyInfo;
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save 
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            Document.DocumentElement.AppendChild(
+                Document.ImportNode(xmlDigitalSignature, true));
+            return Document.OuterXml;
+        }
+        
+    }
+}
